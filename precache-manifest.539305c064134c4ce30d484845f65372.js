2b8679da4e866b7b0b09cf53802005baefd2fd7e
dEnchanter <afolabiopaleye@gmail.com>
2020-05-17 21:42:14 +0100

    Updates

---
+++ precache-manifest.539305c064134c4ce30d484845f65372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527431088cd2d326891e65847fb77f6a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8b0d3417538e7508dbd4",
+    "url": "/monsters-rolodex/static/css/main.a9641cbc.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0d3417538e7508dbd4",
+    "url": "/monsters-rolodex/static/js/main.2e5e4aad.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
